cfdb93273ebfb74896d6d441ca1e29042b4d1303
Frank Kulak <fs.kulak@gmail.com>
2023-05-15 10:06:13 -0400

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,21 +1,46 @@
 import type { SubpageIndex } from "src/global";
 
+// FIXME: Use actual URL or put in config/env variables
 const BASE_URL = "http://127.0.0.1:5500";
 
+const INDEX_CACHE = new Map<string, SubpageIndex>();
+const CONTENT_CACHE = new Map<string, string>();
+
+/**
+ * Fetches the index for the given page.
+ * 
+ * @param basePage Base page to fetch index for
+ */
 export async function fetchPageIndex(basePage: string): Promise<SubpageIndex | undefined> {
   try {
-    const response = await fetch(`${BASE_URL}/${basePage}/_index.json`);
+    if (INDEX_CACHE.has(basePage)) return INDEX_CACHE.get(basePage);
+    const url = `${BASE_URL}/${basePage}/_index.json`;
+    const response = await fetch(url);
     const json = await response.json();
+    if (!Array.isArray(json?.pages))
+      throw Error("Did not find expected JSON structure");
+    INDEX_CACHE.set(basePage, json.pages as SubpageIndex);
     return json.pages;
   } catch (e) {
     console.error(`Could not fetch index for "${basePage}" [${e}]`);
   }
 }
 
+/**
+ * Fetches the HTML content for the given page/endpoint.
+ * 
+ * @param basePage Base page that the content is found on
+ * @param endpoint Endpoint for specific content to fetch
+ */
 export async function fetchPageContent(basePage: string, endpoint: string): Promise<string | undefined> {
   try {
-    const response = await fetch(`${BASE_URL}/${basePage}/${endpoint}.html`);
-    return await response.text();
+    const url = `${BASE_URL}/${basePage}/${endpoint}.html`;
+    if (CONTENT_CACHE.has(url)) return CONTENT_CACHE.get(url);
+    const response = await fetch(url);
+    if (!response.ok) return;
+    const text = await response.text();
+    CONTENT_CACHE.set(url, text);
+    return text;
   } catch (e) {
     console.error(`Could not fetch content for "${basePage}/${endpoint}" [${e}]`);
   }
